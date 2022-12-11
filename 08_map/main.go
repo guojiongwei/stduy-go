4a8751211e9b1a5093774df9175769e2d1a2708e
guojiongwei <g17637907938@163.com>
2022-12-11 10:49:11 +0800

    feat: 学习map的增加删除和遍历操作

--- 08_map/main.go
+++ 08_map/main.go
@@ -4,6 +4,15 @@ import (
 	"fmt"
 )
 
+/** 遍历map */
+func rangeMap(map5 map[int]string) {
+	map5[2] = "2"
+	for index, value := range map5 {
+		fmt.Println("index=", index)
+		fmt.Println("value=", value)
+	}
+}
+
 func main() {
 
 	/** 声明map1， key是string，value是string */
@@ -31,5 +40,12 @@ func main() {
 	var map4 = make(map[int]string, 3)
 	map4[1] = "a"
 
+	fmt.Println("map4=", map4)
+	/** 删除map值 */
+	delete(map4, 1)
+	fmt.Println("map4=", map4)
+	map4[3] = "3"
+	/** map类型是引用传递 */
+	rangeMap(map4)
 	fmt.Println("map4=", map4)
 }
